fe5ce79943be762affb212e1206b109a24a747ed
Eduard CLOETE <ecloete@e5r2p4.wethinkcode.co.za>
2018-08-16 11:03:34 +0200

    Added a new router and market thats able to communicate

--- fixmemarket/src/main/java/MainMarket.java
+++ fixmemarket/src/main/java/MainMarket.java
@@ -1,46 +1,85 @@
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.concurrent.Future;
-import java.nio.channels.AsynchronousSocketChannel;
-import java.util.concurrent.ExecutionException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.nio.charset.Charset;
+import java.util.concurrent.Future;
+//from   w w  w  . j av a 2 s  .  c om
 
 public class MainMarket {
+    public static void main(String[] args) throws Exception {
+        AsynchronousSocketChannel channel = AsynchronousSocketChannel.open();
+        SocketAddress serverAddr = new InetSocketAddress("localhost", 8989);
+        Future<Void> result = channel.connect(serverAddr);
+        result.get();
+        System.out.println("Connected");
+        Attachment attach = new Attachment();
+        attach.channel = channel;
+        attach.buffer = ByteBuffer.allocate(2048);
+        attach.isRead = true;
+        attach.mainThread = Thread.currentThread();
 
-    public static void main (String [] args)
-            throws Exception {
+        Charset cs = Charset.forName("UTF-8");
+        String msg = "Hello";
+        byte[] data = msg.getBytes(cs);
+        attach.buffer.put(data);
+        attach.buffer.flip();
 
-        new MainMarket().go();
+        ReadWriteHandler readWriteHandler = new ReadWriteHandler();
+        channel.write(attach.buffer, attach, readWriteHandler);
+        attach.mainThread.join();
     }
-
-    private void go()
-            throws IOException, InterruptedException, ExecutionException {
-
-        AsynchronousSocketChannel client = AsynchronousSocketChannel.open();
-        InetSocketAddress hostAddress = new InetSocketAddress("localhost", 5000);
-        Future future = client.connect(hostAddress);
-        future.get(); // returns null
-
-        System.out.println("Client is started: " + client.isOpen());
-        System.out.println("Sending messages to server: ");
-
-        String [] messages = new String [] {"Time goes fast.", "What now?", "Bye."};
-
-        for (int i = 0; i < messages.length; i++) {
-
-            byte [] message = new String(messages [i]).getBytes();
-            ByteBuffer buffer = ByteBuffer.wrap(message);
-            Future result = client.write(buffer);
-
-            while (! result.isDone()) {
-                System.out.println("... ");
+}
+class Attachment {
+    AsynchronousSocketChannel channel;
+    ByteBuffer buffer;
+    Thread mainThread;
+    boolean isRead;
+}
+class ReadWriteHandler implements CompletionHandler<Integer, Attachment> {
+    @Override
+    public void completed(Integer result, Attachment attach) {
+        if (attach.isRead) {
+            attach.buffer.flip();
+            Charset cs = Charset.forName("UTF-8");
+            int limits = attach.buffer.limit();
+            byte bytes[] = new byte[limits];
+            attach.buffer.get(bytes, 0, limits);
+            String msg = new String(bytes, cs);
+            System.out.format("Server Responded: "+ msg);
+            try {
+                msg = this.getTextFromUser();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-            System.out.println(messages [i]);
-            buffer.clear();
-            Thread.sleep(3000);
-        } // for
-
-        client.close();
+            if (msg.equalsIgnoreCase("bye")) {
+                attach.mainThread.interrupt();
+                return;
+            }
+            attach.buffer.clear();
+            byte[] data = msg.getBytes(cs);
+            attach.buffer.put(data);
+            attach.buffer.flip();
+            attach.isRead = false; // It is a write
+            attach.channel.write(attach.buffer, attach, this);
+        }else {
+            attach.isRead = true;
+            attach.buffer.clear();
+            attach.channel.read(attach.buffer, attach, this);
+        }
+    }
+    @Override
+    public void failed(Throwable e, Attachment attach) {
+        e.printStackTrace();
+    }
+    private String getTextFromUser() throws Exception{
+        System.out.print("Please enter a  message  (Bye  to quit):");
+        BufferedReader consoleReader = new BufferedReader(
+                new InputStreamReader(System.in));
+        String msg = consoleReader.readLine();
+        return msg;
     }
 }
--- fixmerouter/src/main/java/MainRouter.java
+++ fixmerouter/src/main/java/MainRouter.java
@@ -1,62 +1,108 @@
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.nio.ByteBuffer;
-import java.util.concurrent.Future;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
-import java.util.concurrent.ExecutionException;
-import java.net.InetSocketAddress;
+import java.nio.channels.CompletionHandler;
+import java.nio.charset.Charset;
 
 public class MainRouter {
+    public static void main(String[] args) throws Exception {
+        AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel
+                .open();//w w w  .  j  a  v  a2s .com
+        String host = "localhost";
+        int port = 8989;
+        InetSocketAddress sAddr = new InetSocketAddress(host, port);
+        server.bind(sAddr);
+        System.out.format("Server is listening at %s%n", sAddr);
+        Attachment attach = new Attachment();
+        attach.server = server;
+        server.accept(attach, new ConnectionHandler());
+        Thread.currentThread().join();
+    }
+}
+class Attachment {
+    AsynchronousServerSocketChannel server;
+    AsynchronousSocketChannel client;
+    ByteBuffer buffer;
+    SocketAddress clientAddr;
+    boolean isRead;
+}
 
-    public static void main (String [] args)
-            throws Exception {
-
-        new MainRouter().go();
+class ConnectionHandler implements
+        CompletionHandler<AsynchronousSocketChannel, Attachment> {
+    @Override
+    public void completed(AsynchronousSocketChannel client, Attachment attach) {
+        try {
+            SocketAddress clientAddr = client.getRemoteAddress();
+            System.out.format("Accepted a  connection from  %s%n", clientAddr);
+            attach.server.accept(attach, this);
+            ReadWriteHandler rwHandler = new ReadWriteHandler();
+            Attachment newAttach = new Attachment();
+            newAttach.server = attach.server;
+            newAttach.client = client;
+            newAttach.buffer = ByteBuffer.allocate(2048);
+            newAttach.isRead = false;
+            newAttach.clientAddr = clientAddr;
+            client.read(newAttach.buffer, newAttach, rwHandler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void go()
-            throws IOException, InterruptedException, ExecutionException {
+    @Override
+    public void failed(Throwable e, Attachment attach) {
+        System.out.println("Failed to accept a  connection.");
+        e.printStackTrace();
+    }
+}
 
-        AsynchronousServerSocketChannel serverChannel = AsynchronousServerSocketChannel.open();
-        InetSocketAddress hostAddress = new InetSocketAddress("localhost", 5000);
-        serverChannel.bind(hostAddress);
+class ReadWriteHandler implements CompletionHandler<Integer, Attachment> {
+    @Override
+    public void completed(Integer result, Attachment attach) {
+        if (result == -1) {
+            try {
+                attach.client.close();
+                System.out.format("Stopped   listening to the   client %s%n",
+                        attach.clientAddr);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            return;
+        }
 
-        System.out.println("Server channel bound to port: " + hostAddress.getPort());
-        System.out.println("Waiting for client to connect... ");
+        if (attach.isRead) {
+            attach.buffer.flip();
+            int limits = attach.buffer.limit();
+            byte bytes[] = new byte[limits];
+            attach.buffer.get(bytes, 0, limits);
+            Charset cs = Charset.forName("UTF-8");
+            String msg = new String(bytes, cs);
+            System.out.format("Client at  %s  says: %s%n", attach.clientAddr,
+                    msg);
+            attach.isRead = false; // It is a write
+            //Make data readable for the client, for resend
+            attach.buffer.rewind();
+            attach.buffer.clear();
+            byte[] data = msg.getBytes(cs);
+            attach.buffer.put(data);
+            attach.buffer.flip();
+            //send back the data to the respective client
+            attach.client.write(attach.buffer, attach, this);
+            attach.buffer.rewind();
 
-        Future acceptResult = serverChannel.accept();
-        AsynchronousSocketChannel clientChannel = (AsynchronousSocketChannel) acceptResult.get();
+        } else {
+            // Write to the client
+            //attach.client.write(attach.buffer, attach, this);
+            attach.isRead = true;
+            attach.buffer.clear();
+            attach.client.read(attach.buffer, attach, this);
+        }
+    }
 
-        System.out.println("Messages from client: ");
-
-        if ((clientChannel != null) && (clientChannel.isOpen())) {
-
-            while (true) {
-
-                ByteBuffer buffer = ByteBuffer.allocate(32);
-                Future result = clientChannel.read(buffer);
-
-                while (! result.isDone()) {
-                    // do nothing
-                }
-
-                buffer.flip();
-                String message = new String(buffer.array()).trim();
-                System.out.println(message);
-
-                if (message.equals("Bye.")) {
-
-                    break; // while loop
-                }
-
-                buffer.clear();
-
-            } // while()
-
-            clientChannel.close();
-
-        } // end-if
-
-        serverChannel.close();
+    @Override
+    public void failed(Throwable e, Attachment attach) {
+        e.printStackTrace();
     }
 }
