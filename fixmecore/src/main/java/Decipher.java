2beb4a3c5c6bb570fa469461d8debf0caf96af95
Eduard CLOETE <ecloete@e5r2p4.wethinkcode.co.za>
2018-08-23 12:29:15 +0200

    Added decipher and Protocol Object (name to be changed)

--- fixmecore/src/main/java/Decipher.java
+++ fixmecore/src/main/java/Decipher.java
@@ -1,9 +1,8 @@
 
 public class Decipher {
 
-    public static void readToObject(String string){
+    public static void readToObject(String line){
         Protocol protocol = Protocol.newProtocol(line.split("|")[0], line.split("|")[1],
-                Integer.parseInt(line.split("|")[2]), Integer.parseInt(line.split("|")[3]),
-                line.split("|")[4], line.split("|")[5], line.split("|")[6]);
+                line.split("|")[2], line.split("|")[3], line.split("|")[4], line.split("|")[5]);
     }
 }
