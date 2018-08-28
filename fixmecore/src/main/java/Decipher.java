200647fdd1cfd3ab6c381f14beb5b0b8bb7f7681
Eduard CLOETE <ecloete@e5r2p4.wethinkcode.co.za>
2018-08-28 09:55:53 +0200

    some changes to saving the protocol object

--- fixmecore/src/main/java/Decipher.java
+++ fixmecore/src/main/java/Decipher.java
@@ -1,11 +1,14 @@
-
 package fixmecore;
 
 public class Decipher {
-
     public static void readToObject(String line){
-        Protocol protocol = Protocol.newProtocol(line.split("\\|")[0], line.split("\\|")[1],
-                line.split("\\|")[2], line.split("\\|")[3], line.split("\\|")[4], line.split("\\|")[5],
-                line.split("\\|")[6]);
+        String SenderID = ((line.split("\\|")[0]).split("=")[1]);
+        String MarketID = ((line.split("\\|")[1]).split("=")[1]);
+        String OrderType = ((line.split("\\|")[2]).split("=")[1]);
+        String OrderQuantity = ((line.split("\\|")[3]).split("=")[1]);
+        String OrderPrice = ((line.split("\\|")[4]).split("=")[1]);
+        String OrderStatus = ((line.split("\\|")[5]).split("=")[1]);
+        String CheckSum = ((line.split("\\|")[6]).split("=")[1]);
+        Protocol protocol = Protocol.newProtocol(SenderID, MarketID, OrderType, OrderQuantity, OrderPrice, OrderStatus, CheckSum);
     }
 }
--- fixmecore/src/main/java/Protocol.java
+++ fixmecore/src/main/java/Protocol.java
@@ -1,4 +1,3 @@
-
 package fixmecore;
 
 import lombok.Getter;
@@ -16,24 +15,24 @@ public class Protocol {
     public static String ORDER_STATUS;
     public static String CHECKSUM;
 
-    public Protocol(String sENDER_ID, String mARKET_ID,
-                                String oRDER_TYPE, String oRDER_QUANTITY,
-                                String oRDER_PRICE, String oRDER_STATUS, String cHECKSUM){
-        SENDER_ID = sENDER_ID;
-        MARKET_ID = mARKET_ID;
-        ORDER_TYPE = oRDER_TYPE;
-        ORDER_QUANTITY = oRDER_QUANTITY;
-        ORDER_PRICE = oRDER_PRICE;
-        ORDER_STATUS = oRDER_STATUS;
-        CHECKSUM = cHECKSUM;
+    public Protocol(String sender_id, String market_id,
+                    String order_type, String order_quantity,
+                    String order_price, String order_status, String checksum){
+        SENDER_ID = sender_id;
+        MARKET_ID = market_id;
+        ORDER_TYPE = order_type;
+        ORDER_QUANTITY = order_quantity;
+        ORDER_PRICE = order_price;
+        ORDER_STATUS = order_status;
+        CHECKSUM = checksum;
 
     }
 
-    public static Protocol newProtocol(String sENDER_ID, String mARKET_ID,
-                                String oRDER_TYPE, String oRDER_QUANTITY,
-                                String oRDER_PRICE, String oRDER_STATUS,
-                                String cHECKSUM){
+    public static Protocol newProtocol(String sender_id, String market_id,
+                                       String order_type, String order_quantity,
+                                       String order_price, String order_status,
+                                       String checksum){
 
-        return(new Protocol(sENDER_ID, mARKET_ID, oRDER_TYPE, oRDER_QUANTITY, oRDER_PRICE, oRDER_STATUS, cHECKSUM));
+        return(new Protocol(sender_id, market_id, order_type, order_quantity, order_price, order_status, checksum));
     }
 }
