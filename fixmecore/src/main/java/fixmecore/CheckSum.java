1d30e7f6a47433c2e4ea4cab5dc02b90ae99c12c
Eduard CLOETE <ecloete@e5r2p4.wethinkcode.co.za>
2018-08-30 12:16:35 +0200

    fixed messages

--- fixmecore/src/main/java/fixmecore/CheckSum.java
+++ fixmecore/src/main/java/fixmecore/CheckSum.java
@@ -7,13 +7,13 @@ public class CheckSum {
         String[] mssg_parts = message.split("\\|");
 
         String SENDER_ID = mssg_parts[0];
-        String MARKET_ID = mssg_parts[1];
-        String ORDER_TYPE = mssg_parts[2];
-        String ORDER_QUANTITY = mssg_parts[3];
+        String ORDER_TYPE = mssg_parts[1];
+        String ORDER_QUANTITY = mssg_parts[2];
+        String MARKET_ID = mssg_parts[3];
         String ORDER_PRICE = mssg_parts[4];
         String ORDER_STATUS = mssg_parts[5];
 
-        String checksum = SENDER_ID + separator + MARKET_ID + separator + ORDER_TYPE + separator + fixmecore.Encrypt.encrypt(ORDER_QUANTITY + separator + ORDER_PRICE + separator + ORDER_STATUS);
+        String checksum = SENDER_ID + separator + ORDER_TYPE + separator + MARKET_ID + separator + fixmecore.Encrypt.encrypt(ORDER_QUANTITY + separator + ORDER_PRICE + separator + ORDER_STATUS);
         return (checksum);
     }
 }
